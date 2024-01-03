db38c1f3ff7b9e6e6b83c524bc3d71d91f1178f8
Eric-MiLin-Chen <2479880643@qq.com>
2024-01-03 19:31:57 +0800

    Update flask

--- flask/src/app.py
+++ flask/src/app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 from config_manager import ConfigManager
 from db_manager import DBManager
 from auth_manager import AuthManager
-from student_manager import StudentManager
-import psycopg2
+from user_manager import UserManager
 
 app = Flask(__name__)
 
@@ -15,7 +14,7 @@ app.config["SECRET_KEY"] = config_manager.get_secret_key()
 # 初始化管理器
 db_manager = DBManager(db_params)
 auth_manager = AuthManager(app.config["SECRET_KEY"])
-student_manager = StudentManager(db_manager)
+user_manager = UserManager(db_manager, auth_manager)
 
 
 # 登录路由
@@ -23,7 +22,6 @@ student_manager = StudentManager(db_manager)
 @db_manager.connect_db
 def login(cursor):
     data = request.get_json()
-    print(data)
     username = data["login_info"]["username"]
     password = data["login_info"]["password"]
 
@@ -31,14 +29,16 @@ def login(cursor):
         return jsonify({"message": "Missing username or password"}), 400
 
     try:
-        query = "SELECT P.mm FROM P WHERE P.xh = %s"
-        cursor.execute(query, (username,))
-        rows = cursor.fetchall()
-        if len(rows) > 0 and rows[0][0].strip() == password:
+        user_type = user_manager.verify_credentials(cursor, username, password)
+        if user_type is not None:
+            user_info = user_manager.get_user_info(cursor, user_type, username)
             return jsonify(
                 {
                     "status": "success",
-                    "Authorization": auth_manager.generate_token(username),
+                    "Authorization": auth_manager.generate_token(
+                        username, user_info["user_info"]["role"]
+                    ),
+                    **user_info,
                 }
             )
         else:
@@ -49,30 +49,48 @@ def login(cursor):
 
 
 # 学生选课路由
-@app.route("/student_enroll/", methods=["POST"])
-@auth_manager.token_required
+@app.route("/student_enroll/", methods=["POST"], endpoint="/student_enroll/s")
+@auth_manager.token_required("student")
 @db_manager.connect_db
 def student_enroll(current_user):
+    # 获取前端发送的 JSON 表单
     data = request.get_json()
     xh = current_user
     kch = data["course_info"]["kch"]
     jsgh = data["course_info"]["jsgh"]
 
-    if not kch or not jsgh:
-        return jsonify({"message": "Missing course ID or teacher ID"}), 400
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
+        return jsonify({"status": "failed", "message": "Invalid request format"})
 
-    try:
-        response = student_manager.enroll_student(xh, kch, jsgh)
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        partial_schedule = user_manager.get_partial_schedule(
+            start_position=0,
+            kch=data["course_info"]["kch"],
+            kcm=data["course_info"]["kcm"],
+            xf=data["course_info"]["xf"],
+            jsh=data["course_info"]["jsh"],
+            jsxm=data["course_info"]["jsxm"],
+            sksj=data["course_info"]["sksj"],
+        )
+        return partial_schedule
+
+    elif action == "enroll":
+        # 选课请求
+        response = user_manager.enroll_student(
+            data["course_info"]["kch"], data["course_info"]["jsgh"]
+        )
         return response
-    except psycopg2.errors.UniqueViolation:
-        return jsonify({"message": "Already enrolled in this course"}), 400
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+
+    return jsonify({"status": "failed", "message": "Invalid action"})
 
 
 # 学生退课路由
-@app.route("/drop_course/", methods=["POST"])
-@auth_manager.token_required
+@app.route("/drop_course/", methods=["POST"], endpoint="/drop_course/")
+@auth_manager.token_required("student")
 @db_manager.connect_db
 def drop_course(cursor, current_user):
     data = request.get_json()
@@ -80,48 +98,65 @@ def drop_course(cursor, current_user):
     kch = data["course_info"]["kch"]  # 课程号
     jsgh = data["course_info"]["jsgh"]  # 教师号
 
-    if not kch or not jsgh:
-        return jsonify({"message": "Missing course ID or teacher ID"}), 400
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
+        return jsonify({"status": "failed", "message": "Invalid request format"})
 
-    try:
-        response = student_manager.drop_course(cursor, xh, kch, jsgh)
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        enrolled_courses = user_manager.get_enrolled_courses()
+        return jsonify(
+            {"total_count": len(enrolled_courses), "course_info": enrolled_courses}
+        )
+
+    elif action == "drop":
+        # 选课请求
+        response = user_manager.drop_course(
+            data["course_info"]["kch"],
+            data["course_info"]["jsgh"],
+        )
         return response
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+
+    return jsonify({"status": "failed", "message": "Invalid action"})
 
 
 @app.route("/get_schedule/", methods=["GET", "POST"], endpoint="/get_schedule/")
-@auth_manager.token_required
+@auth_manager.token_required("student")
 @db_manager.connect_db
 def get_schedule(cursor, current_user):
     xh = current_user
     # 调用已有的函数获取已选课程信息
-    enrolled_courses = student_manager.get_enrolled_courses(cursor, xh)
+    enrolled_courses = user_manager.get_enrolled_courses(cursor, xh)
 
     # 返回已选课程信息的 JSON 响应
-    return jsonify({"enrolled_courses": enrolled_courses})
+    return jsonify({"course_info": enrolled_courses})
 
 
 # 教师用户接口
 @app.route("/teacher_schedule/", methods=["GET", "POST"], endpoint="/teacher_schedule/")
-@auth_manager.token_required
+@auth_manager.token_required("teacher")
 @db_manager.connect_db
 def get_teacher_schedule(cursor, current_user):
-    # 在此实现教师课表查询功能，使用 cursor 进行数据库操作
-    pass
+    jsgh = current_user
+    # 调用已有的函数获取已选课程信息
+    enrolled_courses = user_manager.get_teacher_schedule(cursor, jsgh)
+
+    # 返回已选课程信息的 JSON 响应
+    return jsonify(enrolled_courses)
 
 
 # 管理员用户接口
 @app.route("/manage_course/", methods=["GET", "POST"], endpoint="/manage_course/")
-@auth_manager.token_required
+@auth_manager.token_required("admin")
 @db_manager.connect_db
 def manage_course(cursor, current_user):
-    # 在此实现增删查改课程功能，包括课程容量和锁课，使用 cursor 进行数据库操作
     pass
 
 
-@app.route("/manage_student/", methods=["GET", "POST"], endpoint="/manage_course/")
-@auth_manager.token_required
+@app.route("/manage_student/", methods=["GET", "POST"], endpoint="/manage_student/")
+@auth_manager.token_required("admin")
 @db_manager.connect_db
 def manage_student(cursor, current_user):
     # 在此实现增删查改学生功能，使用 cursor 进行数据库操作
@@ -129,9 +164,11 @@ def manage_student(cursor, current_user):
 
 
 @app.route(
-    "/manage_student_course/", methods=["GET", "POST"], endpoint="/manage_course/"
+    "/manage_student_course/",
+    methods=["GET", "POST"],
+    endpoint="/manage_student_course/",
 )
-@auth_manager.token_required
+@auth_manager.token_required("admin")
 @db_manager.connect_db
 def manage_student_course(cursor, current_user):
     # 在此实现增删查改学生选择课程功能，使用 cursor 进行数据库操作
