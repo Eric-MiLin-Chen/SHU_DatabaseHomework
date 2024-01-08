4e47c0b1f2f57e90ecc7a7162f9de6d5312676b5
Eric-MiLin-Chen <2479880643@qq.com>
2024-01-08 19:21:48 +0800

    Update flask

--- flask/src/app.py
+++ flask/src/app.py
@@ -26,9 +26,15 @@ def login(cursor):
     data = request.get_json()
     username = data["login_info"]["username"]
     password = data["login_info"]["password"]
+    print(data)
     if not username or not password:
         return (
-            jsonify({"status": "failed", "message": "Missing username or password"}),
+            jsonify(
+                {
+                    "status": "failed",
+                    "message": "Missing username or password",
+                }
+            ),
             400,
         )
 
@@ -51,14 +57,34 @@ def login(cursor):
                 }
             )
         else:
-            return jsonify({"status": "failed", "message": "Invalid credentials"}), 401
+            return (
+                jsonify(
+                    {
+                        "status": "failed",
+                        "message": "Invalid credentials",
+                    }
+                ),
+                401,
+            )
 
     except Exception as e:
-        return jsonify({"status": "failed", "message": str(e)}), 500
+        return (
+            jsonify(
+                {
+                    "status": "failed",
+                    "message": str(e),
+                }
+            ),
+            500,
+        )
 
 
 # 学生选课路由
-@app.route("/student_enroll/", methods=["POST"], endpoint="/student_enroll/")
+@app.route(
+    "/student_enroll/",
+    methods=["POST"],
+    endpoint="/student_enroll/",
+)
 @auth_manager.token_required("student")
 @db_manager.connect_db
 def student_enroll(cursor, current_user):
@@ -69,16 +95,286 @@ def student_enroll(cursor, current_user):
 
     # 判断是课程查询请求还是选课请求
     if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        partial_schedule = user_manager.get_partial_open_course(
+            cursor=cursor,
+            start_position=0,
+            length=40,
+            kch=data["course_info"]["kch"],
+            kcm=data["course_info"]["kcm"],
+            xf=data["course_info"]["xf"],
+            jsh=data["course_info"]["jsh"],
+            jsxm=data["course_info"]["jsxm"],
+            sksj=data["course_info"]["sksj"],
+        )
+        print(partial_schedule)
+        return partial_schedule
+
+    elif action == "enroll":
+        # 选课请求
+        response = user_manager.enroll_student_course(
+            cursor,
+            xh=xh,
+            kch=data["course_info"]["kch"],
+            jsh=data["course_info"]["jsh"],
+        )
+        return response
+
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+# 学生退课路由
+@app.route(
+    "/student_drop/",
+    methods=["POST"],
+    endpoint="/student_drop/",
+)
+@auth_manager.token_required("student")
+@db_manager.connect_db
+def student_drop(cursor, current_user):
+    data = request.get_json()
+    xh = current_user  # 当前登录的学生学号
+
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        enrolled_courses = user_manager.get_student_enrolled_courses(
+            cursor=cursor, xh=xh
+        )
+        return enrolled_courses
+
+    elif action == "drop":
+        # 选课请求
+        response = user_manager.drop_course(
+            cursor=cursor,
+            xh=xh,
+            kch=data["course_info"]["kch"],
+            jsh=data["course_info"]["jsh"],
+        )
+        return response
+
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+# 学生查课路由
+@app.route(
+    "/get_student_schedule/",
+    methods=["GET", "POST"],
+    endpoint="/get_student_schedule/",
+)
+@auth_manager.token_required("student")
+@db_manager.connect_db
+def get_student_schedule(cursor, current_user):
+    data = request.get_json()
+    xh = current_user
+    if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 调用已有的函数获取已选课程信息
+        enrolled_courses = user_manager.get_student_enrolled_courses(cursor, xh)
+        # 返回已选课程信息的 JSON 响应
+        return enrolled_courses
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+# 教师接口
+# 教师查课路由
+@app.route(
+    "/get_teacher_schedule/",
+    methods=["GET", "POST"],
+    endpoint="/get_teacher_schedule/",
+)
+@auth_manager.token_required("teacher")
+@db_manager.connect_db
+def get_teacher_schedule(cursor, current_user):
+    data = request.get_json()
+    jsgh = current_user
+    if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        enrolled_courses = user_manager.get_teacher_enrolled_courses(cursor, jsgh)
+        return enrolled_courses
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+# 管理员用户接口
+@app.route(
+    "/manage_course_enroll/",
+    methods=["GET", "POST"],
+    endpoint="/manage_course_enroll/",
+)
+@auth_manager.token_required("admin")
+@db_manager.connect_db
+def manage_course_enroll(cursor, current_user):
+    # 获取前端发送的 JSON 表单
+    data = request.get_json()
+
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        partial_schedule = user_manager.get_partial_course(
+            cursor=cursor,
+            start_position=0,
+            length=40,
+            kch=data["course_info"]["kch"],
+            kcm=data["course_info"]["kcm"],
+            xf=data["course_info"]["xf"],
+        )
+        return partial_schedule
+    elif action == "enroll":
+        # 选课请求
+        response = user_manager.enroll_teacher_course(
+            cursor,
+            jsgh=data["user_info"]["id"],
+            kch=data["course_info"]["kch"],
+            sksj=data["course_info"]["sksj"],
+        )
+        return response
+
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+@app.route(
+    "/manage_course_drop/",
+    methods=["GET", "POST"],
+    endpoint="/manage_course_drop/",
+)
+@auth_manager.token_required("admin")
+@db_manager.connect_db
+def manage_course_drop(cursor, current_user):
+    data = request.get_json()
+
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
+
+    action = data["action"]
+
+    if action == "get_schedule":
+        # 课程查询请求
+        enrolled_courses = user_manager.get_teacher_enrolled_courses(
+            cursor=cursor,
+            jsgh=data["user_info"]["id"],
+        )
+        return enrolled_courses
+
+    elif action == "drop":
+        # 选课请求
+        response = user_manager.drop_teacher_course(
+            cursor=cursor,
+            jsgh=data["user_info"]["id"],
+            kch=data["course_info"]["kch"],
+            sksj=data["course_info"]["sksj"],
+        )
+        return response
+
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
+
+
+@app.route(
+    "/manage_student_course_enroll/",
+    methods=["GET", "POST"],
+    endpoint="/manage_student_course_enroll/",
+)
+@auth_manager.token_required("admin")
+@db_manager.connect_db
+def manage_student_course_enroll(cursor, current_user):
+    # 获取前端发送的 JSON 表单
+    data = request.get_json()
+
+    # 判断是课程查询请求还是选课请求
+    if "action" not in data:
         return jsonify({"status": "failed", "message": "Invalid request format"})
 
     action = data["action"]
 
     if action == "get_schedule":
         # 课程查询请求
-        partial_schedule = user_manager.get_partial_schedule(
+        partial_schedule = user_manager.get_partial_course(
             cursor=cursor,
             start_position=0,
-            length=20,
+            length=40,
             kch=data["course_info"]["kch"],
             kcm=data["course_info"]["kcm"],
             xf=data["course_info"]["xf"],
@@ -93,7 +389,7 @@ def student_enroll(cursor, current_user):
         # 选课请求
         response = user_manager.enroll_student(
             cursor,
-            xh=xh,
+            xh=data["user_info"]["id"],
             kch=data["course_info"]["kch"],
             jsh=data["course_info"]["jsh"],
         )
@@ -102,101 +398,51 @@ def student_enroll(cursor, current_user):
     return jsonify({"status": "failed", "message": "Invalid action"})
 
 
-# 学生退课路由
-@app.route("/drop_course/", methods=["POST"], endpoint="/drop_course/")
-@auth_manager.token_required("student")
+@app.route(
+    "/manage_student_course_drop/",
+    methods=["GET", "POST"],
+    endpoint="/manage_student_course_drop/",
+)
+@auth_manager.token_required("admin")
 @db_manager.connect_db
-def drop_course(cursor, current_user):
+def manage_student_course_drop(cursor, current_user):
     data = request.get_json()
-    xh = current_user  # 当前登录的学生学号
 
     # 判断是课程查询请求还是选课请求
     if "action" not in data:
-        return jsonify({"status": "failed", "message": "Invalid request format"})
+        return jsonify(
+            {
+                "status": "failed",
+                "message": "Invalid request format",
+            }
+        )
 
     action = data["action"]
 
     if action == "get_schedule":
         # 课程查询请求
-        enrolled_courses = user_manager.get_enrolled_courses(cursor=cursor, xh=xh)
+        enrolled_courses = user_manager.get_teacher_s(
+            cursor=cursor,
+            xh=data["user_info"]["id"],
+        )
         return enrolled_courses
 
     elif action == "drop":
-        # 选课请求
-        response = user_manager.drop_course(
+        # 退课请求
+        response = user_manager.drop_student_course(
             cursor=cursor,
-            xh=xh,
+            jsgh=data["user_info"]["id"],
             kch=data["course_info"]["kch"],
             jsh=data["course_info"]["jsh"],
         )
         return response
 
-    return jsonify({"status": "failed", "message": "Invalid action"})
-
-
-@app.route("/get_schedule/", methods=["GET", "POST"], endpoint="/get_schedule/")
-@auth_manager.token_required("student")
-@db_manager.connect_db
-def get_schedule(cursor, current_user):
-    data = request.get_json()
-    xh = current_user
-    if "action" not in data:
-        return jsonify({"status": "failed", "message": "Invalid request format"})
-
-    action = data["action"]
-
-    if action == "get_schedule":
-        # 调用已有的函数获取已选课程信息
-        enrolled_courses = user_manager.get_enrolled_courses(cursor, xh)
-        # 返回已选课程信息的 JSON 响应
-        return enrolled_courses
-    return jsonify({"status": "failed", "message": "Invalid action"})
-
-
-# 教师用户接口
-@app.route("/teacher_schedule/", methods=["GET", "POST"], endpoint="/teacher_schedule/")
-@auth_manager.token_required("teacher")
-@db_manager.connect_db
-def get_teacher_schedule(cursor, current_user):
-    data = request.get_json()
-    jsgh = current_user
-    if "action" not in data:
-        return jsonify({"status": "failed", "message": "Invalid request format"})
-
-    action = data["action"]
-
-    if action == "get_schedule":
-        enrolled_courses = user_manager.get_teacher_schedule(cursor, jsgh)
-        return enrolled_courses
-    return jsonify({"status": "failed", "message": "Invalid action"})
-
-
-# 管理员用户接口
-@app.route("/manage_course/", methods=["GET", "POST"], endpoint="/manage_course/")
-@auth_manager.token_required("admin")
-@db_manager.connect_db
-def manage_course(cursor, current_user):
-    pass
-
-
-@app.route("/manage_student/", methods=["GET", "POST"], endpoint="/manage_student/")
-@auth_manager.token_required("admin")
-@db_manager.connect_db
-def manage_student(cursor, current_user):
-    # 在此实现增删查改学生功能，使用 cursor 进行数据库操作
-    pass
-
-
-@app.route(
-    "/manage_student_course/",
-    methods=["GET", "POST"],
-    endpoint="/manage_student_course/",
-)
-@auth_manager.token_required("admin")
-@db_manager.connect_db
-def manage_student_course(cursor, current_user):
-    # 在此实现增删查改学生选择课程功能，使用 cursor 进行数据库操作
-    pass
+    return jsonify(
+        {
+            "status": "failed",
+            "message": "Invalid action",
+        }
+    )
 
 
 if __name__ == "__main__":
--- flask/src/user_manager.py
+++ flask/src/user_manager.py
@@ -80,9 +80,9 @@ class UserManager:
             },
         }
 
-    # 其他方法，例如 enroll_student, drop_course 等
+    # 学生方法
 
-    def enroll_student(self, cursor, xh, kch, jsh):
+    def enroll_student_course(self, cursor, xh, kch, jsh):
         try:
             insert_query = """
                 INSERT INTO E (xh, kch, jsh)
@@ -98,7 +98,7 @@ class UserManager:
         except psycopg2.errors.UniqueViolation:
             return jsonify({"status": "failed", "message": "UniqueViolation"})
 
-    def drop_course(self, cursor, xh, kch, jsh):
+    def drop_student_course(self, cursor, xh, kch, jsh):
         try:
             delete_query = """
                 DELETE FROM E
@@ -116,7 +116,7 @@ class UserManager:
         except Exception as e:
             return jsonify({"status": "failed", "message": str(e)})
 
-    def get_enrolled_courses(self, cursor, xh):
+    def get_student_enrolled_courses(self, cursor, xh):
         query = """
             SELECT
                 E.kch,
@@ -162,7 +162,7 @@ class UserManager:
             }
         )
 
-    def get_partial_schedule(
+    def get_partial_open_course(
         self,
         cursor,
         start_position,
@@ -279,14 +279,18 @@ class UserManager:
             }
         )
 
-    def get_teacher_schedule(self, cursor, jsgh):
+    # 教师方法
+
+    def get_teacher_enrolled_courses(self, cursor, jsgh):
         query = """
             SELECT
                 C.kch,
                 C.kcm,
                 O.sksj,
                 E.xh AS student_id,
-                S.xm AS student_name
+                S.xm AS student_name,
+                C.xf,
+                C.zdrs
             FROM
                 O
             JOIN
@@ -306,12 +310,14 @@ class UserManager:
 
         teacher_schedule = {}
         for row in rows:
-            kch, kcm, sksj, student_id, student_name = row
+            kch, kcm, sksj, student_id, student_name, xf, zdrs = row
             if kch not in teacher_schedule:
                 teacher_schedule[kch] = {
                     "kch": kch,
                     "kcm": kcm,
                     "sksj": sksj,
+                    "xf": xf,
+                    "zdrs": zdrs,
                     "student_info": [],
                 }
             teacher_schedule[kch]["student_info"].append(
@@ -325,3 +331,192 @@ class UserManager:
                 "course_info": list(teacher_schedule.values()),
             }
         )
+
+    # 管理员方法
+
+    def get_partial_course(
+        self,
+        cursor,
+        start_position,
+        length=20,
+        kch="",
+        kcm="",
+        xf="",
+    ):
+        # 构建 SQL 查询总数的语句
+        count_query = """
+            SELECT
+                COUNT(*)
+            FROM
+                C
+            JOIN
+                O ON C.kch = O.kch
+            JOIN
+                T ON O.jsgh = T.jsgh
+        """
+
+        # 添加约束条件
+        where_conditions = []
+        parameters = {}
+
+        if kch != "":
+            where_conditions.append("C.kch = %(kch)s")
+            parameters["kch"] = kch
+        if kcm != "":
+            where_conditions.append("C.kcm = %(kcm)s")
+            parameters["kcm"] = kcm
+        if xf != "":
+            where_conditions.append("C.xf = %(xf)s")
+            parameters["xf"] = xf
+
+        if where_conditions:
+            count_query += " WHERE " + " AND ".join(where_conditions)
+
+        # 执行总数查询
+        cursor.execute(count_query, parameters)
+        total_count = cursor.fetchone()[0]
+
+        # 构建 SQL 查询分页的语句
+        schedule_query = """
+            SELECT
+                C.kch,
+                C.kcm,
+                O.jsh,
+                T.jsxm,
+                O.sksj,
+                C.xf,
+                C.zdrs
+            FROM
+                C
+            JOIN
+                O ON C.kch = O.kch
+            JOIN
+                T ON O.jsgh = T.jsgh
+        """
+        if where_conditions:
+            schedule_query += " WHERE " + " AND ".join(where_conditions)
+
+        # 添加排序和分页
+        schedule_query += f"""
+            ORDER BY
+                C.kch
+            OFFSET
+                %(start_position)s
+            LIMIT
+                %(length)s;
+        """
+        parameters["start_position"] = start_position
+        parameters["length"] = length
+
+        # 执行分页查询
+        cursor.execute(schedule_query, parameters)
+        rows = cursor.fetchall()
+        partial_schedule = [
+            {
+                "kch": row[0],
+                "kcm": row[1],
+                "xf": row[5],
+                "zdrs": row[6],
+            }
+            for row in rows
+        ]
+
+        # 将总数和分页结果一起返回
+        result = {
+            "total_count": total_count,
+            "course_info": partial_schedule,
+            "status": "success",
+        }
+
+        return result
+
+    def enroll_teacher_course(self, cursor, jsgh, kch, sksj):
+        try:
+            # 判断同教师在相同sksj中是否已有课程
+            check_course_query = "SELECT COUNT(*) FROM O WHERE jsgh = %s AND sksj = %s"
+            cursor.execute(check_course_query, (jsgh, sksj))
+            existing_courses_count = cursor.fetchone()[0]
+
+            if existing_courses_count > 0:
+                return {"status": "error", "message": "Course time conflict"}
+
+            # 查询同课程号下所有开课的jsgh并保存入列表
+            all_jsgh_query = "SELECT jsgh FROM O WHERE kch = %s"
+            cursor.execute(all_jsgh_query, (kch,))
+            all_jsgh_list = [row[0] for row in cursor.fetchall()]
+
+            # 分配教师号，从最小值开始递增，直到找到第一个不冲突的教师号
+            new_jsgh = 1001
+            while new_jsgh in all_jsgh_list:
+                new_jsgh += 1
+
+            # 插入记录到表O
+            insert_query = "INSERT INTO O (jsgh, kch, jsh, sksj) VALUES (%(jsgh)s, %{kch}s, %(jsh)s, %(sksj)s)"
+            parameters = {
+                "jsgh": jsgh,
+                "kch": kch,
+                "jsh": new_jsgh,
+                "sksj": sksj,
+            }
+            cursor.execute(insert_query, parameters)
+
+            return {"status": "success"}
+
+        except psycopg2.errors.UniqueViolation:
+            return jsonify({"status": "failed", "message": "UniqueViolation"})
+
+    # def get_teacher_enrolled_courses(self, cursor, jsgh):
+    #     query = """
+    #         SELECT
+    #             O.kch,
+    #             C.kcm,
+    #             O.sksj,
+    #             C.xf,
+    #             O.jsh,
+    #             C.zdrs
+    #         FROM
+    #             O
+    #         JOIN
+    #             C ON O.kch = C.kch
+    #         WHERE
+    #             O.jsgh = %(jsgh)s;
+    #     """
+    #     parameters = {"jsgh": jsgh}
+    #     cursor.execute(query, parameters)
+    #     rows = cursor.fetchall()
+
+    #     enrolled_courses = [
+    #         {
+    #             "kch": row[0],
+    #             "kcm": row[1],
+    #             "sksj": row[2],
+    #             "xf": row[3],
+    #             "jsh": row[4],
+    #             "zdrs": row[5],
+    #         }
+    #         for row in rows
+    #     ]
+
+    #     return {
+    #         "status": "success",
+    #         "total_count": len(enrolled_courses),
+    #         "course_info": enrolled_courses,
+    #     }
+
+    def drop_teacher_course(self, cursor, jsgh, kch, sksj):
+        try:
+            delete_query = """
+                DELETE FROM O
+                WHERE jsgh = %(jsgh)s AND kch = %(kch)s AND sksj = %(sksj)s;
+            """
+            parameters = {
+                "jsgh": jsgh,
+                "kch": kch,
+                "sksj": sksj,
+            }
+            cursor.execute(delete_query, parameters)
+            if cursor.rowcount == 0:
+                raise Exception("No matching record found for deletion")
+            return jsonify({"status": "success"})
+        except Exception as e:
+            return jsonify({"status": "failed", "message": str(e)})
